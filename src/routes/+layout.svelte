b9e6f82f08a690d540c2b6b0ee10225d299a1dfd
whiteformed <whiteformed@gmail.com>
2026-03-19 20:11:50 +0300

    upd

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,34 @@
 <script lang="ts">
 	import './layout.css';
+	import { page } from '$app/state';
 	import favicon from '$lib/assets/favicon.svg';
+	import type { Snippet } from 'svelte';
 
-	let { children } = $props();
+	type Props = {
+		children: Snippet;
+	};
+	const { children }: Props = $props();
+
+	function getTitleByPathName(path: typeof page.url.pathname) {
+		switch (path) {
+			case '/cases/sber-university':
+				return 'Кейс | ☁️ СберУниверситет / Медиатека';
+			case '/cases/market-twits':
+				return 'Кейс | 🌐 MarketTwits / Новостной агрегатор';
+			case '/cases/texol':
+				return 'Кейс | 🛠️ Цифровой инспектор';
+
+			default:
+				return 'Резюме | Валерия Карлова – продуктовый дизайнер';
+		}
+	}
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<svelte:head>
+	<title>{getTitleByPathName(page.url.pathname)}</title>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+
 <div class="py-7">
 	{@render children()}
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,4 @@
-<script>
+<script lang="ts">
 	import HomePage from '$pages/Home/HomePage.svelte';
 </script>
 
